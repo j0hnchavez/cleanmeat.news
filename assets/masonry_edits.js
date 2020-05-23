a15412bce24d88388056021a416292ad785304a7
j0hnchavez <chavezjohn415@gmail.com>
2020-05-23 03:13:49 +0000

    Masonry changes with js rather than jquery

--- assets/masonry_edits.js
+++ assets/masonry_edits.js
@@ -1,9 +1,20 @@
 // external js: masonry.pkgd.js
 
-$('.grid').masonry({
+// jquery
+// not sure if it's working
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 10%,
+//   gutter: 10,
+//   fitWidth: true,
+//   horizontalOrder: true
+// });
+
+// vanilla JS
+var msnry = new Masonry( '.grid', {
+  columnWidth: 200,
   itemSelector: '.grid-item',
-  columnWidth: 10%,
   gutter: 10,
   fitWidth: true,
-  horizontalOrder: true
+  horizontalOrder: true;
 });
