46099c3ab5b0b9981745009fd708fcf679ad7e5c
j0hnchavez <chavezjohn415@gmail.com>
2020-05-23 03:34:24 +0000

    Attempt to learn from codepen

--- assets/masonry_edits.js
+++ assets/masonry_edits.js
@@ -1,20 +1,19 @@
 // external js: masonry.pkgd.js
 
 // jquery
-// not sure if it's working
-// $('.grid').masonry({
-//   itemSelector: '.grid-item',
-//   columnWidth: 10%,
-//   gutter: 10,
-//   fitWidth: true,
-//   horizontalOrder: true
-// });
-
-// vanilla JS
-var msnry = new Masonry( '.grid', {
-  columnWidth: 200,
+$('.grid').masonry({
   itemSelector: '.grid-item',
+  columnWidth: 105,
   gutter: 10,
   fitWidth: true,
-  horizontalOrder: true;
+  horizontalOrder: true
 });
+
+// vanilla JS
+// var msnry = new Masonry( '.grid', {
+//   columnWidth: 200,
+//   itemSelector: '.grid-item',
+//   gutter: 10,
+//   fitWidth: true,
+//   horizontalOrder: true;
+// });
